0a09909b58e016826e19b7237b2a196fafc99cd4
ferlang <75319343+ferlang@users.noreply.github.com>
2020-12-02 13:06:07 +0700

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -7,35 +7,17 @@ client = commands.Bot(command_prefix = ".")
 @client.event
 async def on_ready():
     print("Bot1 Is Ready")
+
+@client.event
+async def on_message(message):
+    bad_words = ["bad", "stop", "45"]
+    for word in bad_words:
+        if message.content.count(word) > 0:
+            print("A bad word was said")
+            await message.channel.purge(limit=1)
 	
 @client.command()
 async def hy(ctx):
 	await ctx.send("Hello")
-	
-@client.command()
-async def server(ctx):
-        name = str(ctx.guild.name)
-        description = str(ctx.guild.description)
-	
-        owner = str(ctx.guild.owner)
-        id = str(ctx.guild.id)
-        region = str(ctx.guild.region)
-        memberCount = str(ctx.guild.member_count)
- 
-        icon = str(ctx.guild.icon_url)
-
-        embed = discord.Embed(
-            title=name + " Server Information",
-            description=description,
-            color=discord.Color.red()
-        )
-        embed.set_thumbnail(url_icon)
-        embed.add_field(name="owner", value=owner, inline=True)
-        embed.add_field(name="Server ID", value=id, inline=True)
-        embed.add_field(name="Region", value=region, inline=True)
-        embed.add_field(name="Member Count", value=memberCount, inline=True)
-
-        await ctx.send(embed=embed)
-	
 	
 client.run(os.environ['DISCORD_TOKEN'])
